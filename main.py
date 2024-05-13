4477524629ecc14d4cc0ac92eb22880ade9a073d
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 14:09:53 +0700

    Response Status Code

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi import status
+from fastapi import Body
+
+from typing import Annotated
+
+from models.item import Item
+
+app = FastAPI()
+
+# Response Status Code
+@app.post("/items", response_model = Item, status_code = status.HTTP_201_CREATED)
+async def create_item(item: Annotated[Item, Body()]):
+  return item
