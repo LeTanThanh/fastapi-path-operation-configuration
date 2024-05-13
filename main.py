d6acc19f79f96fb4c52dad64744ae04d98c80bac
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 14:20:58 +0700

    Description from docstring

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ async def create_item(item: Annotated[Item, Body()]):
 """
 
 # Summary and description
+"""
 @app.post("/items",
   response_model = Item,
   status_code = status.HTTP_201_CREATED,
@@ -47,4 +48,23 @@ async def create_item(item: Annotated[Item, Body()]):
   summary = "Create an item",
   description = "Create an item with all the information, name, description, price, tax and a set of unique tags")
 async def create_item(item: Annotated[Item, Body()]):
+  return item
+"""
+
+# Description from docstring
+@app.post("/items",
+  response_model = Item,
+  status_code = status.HTTP_201_CREATED,
+  tags = [Tags.items],
+  summary = "Create an item")
+async def create_item(item: Annotated[Item, Body()]):
+  """
+  Create an item with all the information:
+
+  - **name**: each item must have a name
+  - **description**: a long description
+  - **price**: required
+  - **tax**: if the item doesn't have tax, you can omit this
+  - **tags**: a set of unique tag strings for this item
+  """
   return item
