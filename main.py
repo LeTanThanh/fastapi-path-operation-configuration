4a443a1d4bbd63babb077f502934aab1d38c67b0
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 14:12:40 +0700

    Tags

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ from models.item import Item
 app = FastAPI()
 
 # Response Status Code
+"""
 @app.post("/items", response_model = Item, status_code = status.HTTP_201_CREATED)
+async def create_item(item: Annotated[Item, Body()]):
+  return item
+"""
+
+# Tags
+@app.post("/items",
+  response_model = Item,
+  status_code = status.HTTP_201_CREATED,
+  tags = ["Items"])
 async def create_item(item: Annotated[Item, Body()]):
   return item
