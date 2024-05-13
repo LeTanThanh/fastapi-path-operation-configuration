e37047911ae640b6942b0aad97e8ddbba03d9393
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 14:15:12 +0700

    Tags with Enums

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import status
 from fastapi import Body
 
 from typing import Annotated
+from enum import Enum
 
 from models.item import Item
 
@@ -16,9 +17,22 @@ async def create_item(item: Annotated[Item, Body()]):
 """
 
 # Tags
+"""
 @app.post("/items",
   response_model = Item,
   status_code = status.HTTP_201_CREATED,
   tags = ["Items"])
+async def create_item(item: Annotated[Item, Body()]):
+  return item
+"""
+
+# Tags with Enums
+class Tags(Enum):
+  items = "Items"
+
+@app.post("/items",
+  response_model = Item,
+  status_code = status.HTTP_201_CREATED,
+  tags = [Tags.items])
 async def create_item(item: Annotated[Item, Body()]):
   return item
